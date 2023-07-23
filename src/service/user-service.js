f7e1f4a5708de86cd088fca257416a83e5821bca
Kukuh182 <kukuhpr21@gmail.com>
2023-07-24 05:40:41 +0700

    add get current user

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,7 +1,7 @@
 import { prismaClient } from "../application/database.js";
 import { logger } from "../application/logging.js";
 import { ResponseError } from "../error/response-error.js";
-import { loginUserValidation, registerUserValidation } from "../validation/user-validation.js"
+import { getUserValidation, loginUserValidation, registerUserValidation } from "../validation/user-validation.js"
 import { validate } from "../validation/validation.js"
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
@@ -66,7 +66,27 @@ const login = async (request) => {
     });
 }
 
+const get = async (username) => {
+    username = validate(getUserValidation, username);
+
+    const user = await prismaClient.user.findUnique({
+        where: {
+            username: username
+        },
+        select: {
+            username: true,
+            name: true
+        }
+    });
+
+    if (!user) {
+        throw new ResponseError(404, "User not found");
+    }
+
+    return user;
+}
 export default {
     register,
-    login
+    login,
+    get
 }
--- test/user.test.js
+++ test/user.test.js
@@ -123,3 +123,32 @@ describe('POST /api/users/login', function () {
         expect(result.body.errors).toBeDefined();
     });
 });
+
+describe('GET /api/users/current', function () {
+    beforeEach(async () => {
+        await createTestUser();
+    });
+    
+    afterEach(async () => {
+        await removeTestUser();
+    });
+
+    it('should can get current user', async () => {
+        let result = await supertest(web)
+            .get('/api/users/current')
+            .set("Authorization", "test");
+        logger.info(result.body);
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe("test");
+        expect(result.body.data.name).toBe("test");
+    });
+    
+    it('should reject if token is invalid', async () => {
+        let result = await supertest(web)
+            .get('/api/users/current')
+            .set("Authorization", "salah");
+        logger.info(result.body);
+        expect(result.status).toBe(401);
+        expect(result.body.errors).toBeDefined();
+    });
+});
