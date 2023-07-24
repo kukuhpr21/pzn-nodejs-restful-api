4b18cd712e00165582dc15ff5195aeb6b69b4303
Kukuh182 <kukuhpr21@gmail.com>
2023-07-24 17:08:06 +0700

    add user logout

--- test/user.test.js
+++ test/user.test.js
@@ -218,3 +218,35 @@ describe('PATCH /api/users/current', function () {
         expect(result.status).toBe(401);
     });
 });
+
+describe('DELETE /api/users/logout', function () { 
+    beforeEach(async () => {
+        await createTestUser();
+    });
+    
+    afterEach(async () => {
+        await removeTestUser();
+    });
+
+    it('should can logout', async () => {
+        let result = await supertest(web)
+            .delete('/api/users/logout')
+            .set("Authorization", "test")
+            .send({});
+        logger.info(result.body);
+        expect(result.status).toBe(200);
+        expect(result.body.data).toBe("OK");
+
+        const user = await getTestUser();
+        expect(user.token).toBe(null);
+    });
+    
+    it('should reject logout if token is invalid', async () => {
+        let result = await supertest(web)
+            .delete('/api/users/logout')
+            .set("Authorization", "salah")
+            .send({});
+        logger.info(result.body);
+        expect(result.status).toBe(401);
+    });
+});
