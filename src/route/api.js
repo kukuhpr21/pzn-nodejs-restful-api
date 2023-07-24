2997adc26c787a77a3e316f7c906f8f09c44fa27
Kukuh182 <kukuhpr21@gmail.com>
2023-07-24 23:01:44 +0700

    add create contact

--- src/route/api.js
+++ src/route/api.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
+import contactController from "../controller/contact-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
 
 const userRouter = new express.Router();
@@ -7,6 +8,7 @@ userRouter.use(authMiddleware)
 userRouter.get("/api/users/current", userController.get);
 userRouter.patch("/api/users/current", userController.update);
 userRouter.delete("/api/users/logout", userController.logout);
+userRouter.post("/api/contact", contactController.create);
 
 export {
     userRouter
